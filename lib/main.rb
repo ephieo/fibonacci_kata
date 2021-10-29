406071bab22ce4f6a1b534554e7084f4808bdf78
Ephie Oyedoh <ephieoyedoh@andrewmacmurray.localdomain>
2021-10-29 15:57:30 +0100

    first attempt at fibonacci kata without recursion

--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,19 @@
-class Fibonacci 
-    
+class Puzzle 
+
+    def fibonacci(num)
+        start_arr = [0,1]
+        while start_arr.size < num
+            sum = start_arr[start_arr.size - 1] + start_arr[start_arr.size - 2]
+            start_arr.push(sum)
+        end
+        
+        puts start_arr
+        return start_arr
+
+    end
+
 end 
+
+puzzle = Puzzle.new
+
+puzzle.fibonacci(3)
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,14 +4,18 @@ SimpleCov.start
 
 require_relative '../lib/main'
 
-describe Fibonacci do 
-    subject(:fibonacci) {described_class.new}
+describe Puzzle do 
+    subject(:puzzle) {described_class.new}
 
     it ' has a class called fibonacci' do
-        expect(fibonacci).to be_instance_of(Fibonacci)
+        expect(puzzle).to be_instance_of(Puzzle)
     end
 
-    # it 'takes an array as an argument and returns third-to-last and second-to-last numbers'do 
-    #    new_arr
-    # end
+    it 'it should take an integer and return the fibonacci sequence up to that integer'do 
+        expect(puzzle.fibonacci(8)).to eq([0,1,1,2,3,5,8,13])
+        expect(puzzle.fibonacci(4)).to eq([0,1,1,2])
+        expect(puzzle.fibonacci(25)).to eq([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368])
+
+    end
+
 end
